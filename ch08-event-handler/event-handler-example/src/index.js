33cc12b9fae577d1a2df8450045ff4d5920617f6
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-06-05 17:25:52 +0900

    이벤트 핸들러: arrow function으로 bind

--- ch08-event-handler/event-handler-example/src/index.js
+++ ch08-event-handler/event-handler-example/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ConfirmButton from './component/ConfirmButton-class-field';
+import ConfirmButton from './component/ConfirmButton-arrow-func';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
