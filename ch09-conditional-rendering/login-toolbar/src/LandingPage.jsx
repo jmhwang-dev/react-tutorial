fb2b7670d7de187ad5dfec877100c9f484888020
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-06-07 10:05:16 +0900

    조건부 렌더링 실습

---
+++ ch09-conditional-rendering/login-toolbar/src/LandingPage.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import Toolbar from "./Toolbar";
+
+function LandingPage(props) {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const onClickLogin = () => {
+        setIsLoggedIn(true);
+    }
+    const onClickLogout = () => {
+        setIsLoggedIn(false);   
+    }
+
+    return (
+        <div>
+            <Toolbar 
+                isLoggedIn={isLoggedIn}
+                onClickLogin={onClickLogin}
+                onClickLogout={onClickLogout}
+            />
+            <div>리액트 공부</div>
+        </div>
+    )
+}
+
+export default LandingPage
