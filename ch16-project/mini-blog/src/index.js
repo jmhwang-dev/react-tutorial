d297ef2d9bb06dc310f9c49bd49a292af5bde15f
jmhwang-dev <jungmin.hwang.dev@gmail.com>
2024-06-13 13:19:54 +0900

    list component

--- ch16-project/mini-blog/src/index.js
+++ ch16-project/mini-blog/src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Button from './component/ui/Button';
-import TextInput from './component/ui/TextInput';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Button />
-    <TextInput />
+    <App />
   </React.StrictMode>
 );
 
